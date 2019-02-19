fddf45f26fb4d7b58ee87173604ec00a72aac319
vijayst <vijayst@gmail.com>
2019-02-19 18:47:04 +0530

    Navbar styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { Icon } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import EventDashboard from './event/EventDashboard';
+import NavBar from './nav/NavBar';
 
 class App extends Component {
     render() {
         return (
-            <div className="app">
-                Hello world
-                <Icon style={{ color: 'red' }} className="arrow alternate circle right" />
-            </div>
+            <>
+                <NavBar />
+                <Container className="main">
+                    <EventDashboard />
+                </Container>
+            </>
         );
     }
 }
---
+++ src/event/EventDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Grid } from 'semantic-ui-react';
+
+export default function EventDashboard() {
+    return (
+        <Grid>
+            <Grid.Column width={10}><h2>Left</h2></Grid.Column>
+            <Grid.Column width={6}><h2>Right</h2></Grid.Column>
+        </Grid>
+    );
+}

e36b0b2bbc91f3cbba5e7411af4adbb64401214d
vijayst <vijayst@gmail.com>
2019-02-19 17:45:51 +0530

    prettierrc

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        Hello world
-      </div>
-    );
-  }
+    render() {
+        return <div className="app">Hello world</div>;
+    }
 }
 
 export default App;

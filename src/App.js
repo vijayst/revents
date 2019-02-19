91a17f6a0a604dc8e4ca0362f9490216ac44b468
vijayst <vijayst@gmail.com>
2019-02-19 17:57:00 +0530

    add semantic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
+import { Icon } from 'semantic-ui-react';
 
 class App extends Component {
     render() {
-        return <div className="app">Hello world</div>;
+        return (
+            <div className="app">
+                Hello world
+                <Icon style={{ color: 'red' }} className="arrow alternate circle right" />
+            </div>
+        );
     }
 }
 

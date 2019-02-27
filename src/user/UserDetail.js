36511e4edd448b4f95286ed08d7c2f19c21e25af
vijayst <vijayst@gmail.com>
2019-02-27 09:49:49 +0530

    user detail page

--- src/user/UserDetail.js
+++ src/user/UserDetail.js
@@ -1,124 +1,186 @@
-import React, {Component} from 'react';
-import {Button, Card, Grid, Header, Icon, Image, Item, List, Menu, Segment} from "semantic-ui-react";
+import React from 'react';
+import {
+    Button,
+    Card,
+    Grid,
+    Header,
+    Icon,
+    Image,
+    Item,
+    List,
+    Menu,
+    Segment
+} from 'semantic-ui-react';
+import { compose } from 'redux';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { format, differenceInYears } from 'date-fns';
 
-class UserDetailedPage extends Component {
+function UserDetailedPage(props) {
+    const {
+        user: {
+            displayName,
+            about,
+            occupation,
+            origin: country,
+            interests,
+            photos,
+            city,
+            createdAt,
+            dateOfBirth
+        }
+    } = props;
+    const age =
+        dateOfBirth && differenceInYears(new Date(), new Date(dateOfBirth));
+    return (
+        <Grid>
+            <Grid.Column width={16}>
+                <Segment>
+                    <Item.Group>
+                        <Item>
+                            <Item.Image
+                                avatar
+                                size="small"
+                                src="https://randomuser.me/api/portraits/men/20.jpg"
+                            />
+                            <Item.Content verticalAlign="bottom">
+                                <Header as="h1">{displayName}</Header>
+                                <br />
+                                {occupation && (
+                                    <Header as="h3">{occupation}</Header>
+                                )}
+                                <br />
+                                <Header as="h3">
+                                    {age}, Lives in {city}
+                                </Header>
+                            </Item.Content>
+                        </Item>
+                    </Item.Group>
+                </Segment>
+            </Grid.Column>
+            <Grid.Column width={12}>
+                <Segment>
+                    <Grid columns={2}>
+                        <Grid.Column width={10}>
+                            <Header
+                                icon="smile"
+                                content={`About ${displayName}`}
+                            />
+                            <p>
+                                I am a: <strong>{occupation || '-'}</strong>
+                            </p>
+                            <p>
+                                Originally from{' '}
+                                <strong>{country || '-'}</strong>
+                            </p>
+                            <p>
+                                Member Since:{' '}
+                                <strong>
+                                    {createdAt &&
+                                        format(
+                                            createdAt.toDate(),
+                                            'Do MMM YYYY'
+                                        )}
+                                </strong>
+                            </p>
+                            <p>{about}</p>
+                        </Grid.Column>
+                        <Grid.Column width={6}>
+                            <Header icon="heart outline" content="Interests" />
+                            <List>
+                                {interests &&
+                                    interests.map(interest => (
+                                        <Item key={interest}>
+                                            <Icon name="heart" />
+                                            <Item.Content>
+                                                {interest}
+                                            </Item.Content>
+                                        </Item>
+                                    ))}
+                            </List>
+                        </Grid.Column>
+                    </Grid>
+                </Segment>
+            </Grid.Column>
+            <Grid.Column width={4}>
+                <Segment>
+                    <Button color="teal" fluid basic content="Edit Profile" />
+                </Segment>
+            </Grid.Column>
 
-    render() {
-
-        return (
-            <Grid>
-                <Grid.Column width={16}>
-                    <Segment>
-                        <Item.Group>
-                            <Item>
-                                <Item.Image avatar size='small' src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                                <Item.Content verticalAlign='bottom'>
-                                    <Header as='h1'>First Name</Header>
-                                    <br/>
-                                    <Header as='h3'>Occupation</Header>
-                                    <br/>
-                                    <Header as='h3'>27, Lives in London, UK</Header>
-                                </Item.Content>
-                            </Item>
-                        </Item.Group>
-
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column width={12}>
-                    <Segment>
-                        <Grid columns={2}>
-                            <Grid.Column width={10}>
-                                <Header icon='smile' content='About Display Name'/>
-                                <p>I am a: <strong>Occupation Placeholder</strong></p>
-                                <p>Originally from <strong>United Kingdom</strong></p>
-                                <p>Member Since: <strong>28th March 2018</strong></p>
-                                <p>Description of user</p>
-
-                            </Grid.Column>
-                            <Grid.Column width={6}>
-
-                                <Header icon='heart outline' content='Interests'/>
-                                <List>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 1</Item.Content>
-                                    </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 2</Item.Content>
-                                    </Item>
-                                    <Item>
-                                        <Icon name='heart'/>
-                                        <Item.Content>Interest 3</Item.Content>
-                                    </Item>
-                                </List>
-                            </Grid.Column>
-                        </Grid>
-
-                    </Segment>
-                </Grid.Column>
-                <Grid.Column width={4}>
-                    <Segment>
-                        <Button color='teal' fluid basic content='Edit Profile'/>
-                    </Segment>
-                </Grid.Column>
-
+            {photos && (
                 <Grid.Column width={12}>
                     <Segment attached>
-                        <Header icon='image' content='Photos'/>
-                        
-                        <Image.Group size='small'>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
-                            <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                        <Header icon="image" content="Photos" />
+
+                        <Image.Group size="small">
+                            {photos.map(p => (
+                                <Image key={p.name} src={p.url} />
+                            ))}
                         </Image.Group>
                     </Segment>
                 </Grid.Column>
+            )}
 
-                <Grid.Column width={12}>
-                    <Segment attached>
-                        <Header icon='calendar' content='Events'/>
-                        <Menu secondary pointing>
-                            <Menu.Item name='All Events' active/>
-                            <Menu.Item name='Past Events'/>
-                            <Menu.Item name='Future Events'/>
-                            <Menu.Item name='Events Hosted'/>
-                        </Menu>
+            <Grid.Column width={12}>
+                <Segment attached>
+                    <Header icon="calendar" content="Events" />
+                    <Menu secondary pointing>
+                        <Menu.Item name="All Events" active />
+                        <Menu.Item name="Past Events" />
+                        <Menu.Item name="Future Events" />
+                        <Menu.Item name="Events Hosted" />
+                    </Menu>
 
-                        <Card.Group itemsPerRow={5}>
+                    <Card.Group itemsPerRow={5}>
+                        <Card>
+                            <Image src={'/assets/drinks.jpg'} />
+                            <Card.Content>
+                                <Card.Header textAlign="center">
+                                    Event Title
+                                </Card.Header>
+                                <Card.Meta textAlign="center">
+                                    28th March 2018 at 10:00 PM
+                                </Card.Meta>
+                            </Card.Content>
+                        </Card>
 
-                            <Card>
-                                <Image src={'/assets/drinks.jpg'}/>
-                                <Card.Content>
-                                    <Card.Header textAlign='center'>
-                                        Event Title
-                                    </Card.Header>
-                                    <Card.Meta textAlign='center'>
-                                        28th March 2018 at 10:00 PM
-                                    </Card.Meta>
-                                </Card.Content>
-                            </Card>
-
-                            <Card>
-                                <Image src={'/assets/drinks.jpg'}/>
-                                <Card.Content>
-                                    <Card.Header textAlign='center'>
-                                        Event Title
-                                    </Card.Header>
-                                    <Card.Meta textAlign='center'>
-                                        28th March 2018 at 10:00 PM
-                                    </Card.Meta>
-                                </Card.Content>
-                            </Card>
-
-                        </Card.Group>
-                    </Segment>
-                </Grid.Column>
-            </Grid>
-
-        );
-    }
+                        <Card>
+                            <Image src={'/assets/drinks.jpg'} />
+                            <Card.Content>
+                                <Card.Header textAlign="center">
+                                    Event Title
+                                </Card.Header>
+                                <Card.Meta textAlign="center">
+                                    28th March 2018 at 10:00 PM
+                                </Card.Meta>
+                            </Card.Content>
+                        </Card>
+                    </Card.Group>
+                </Segment>
+            </Grid.Column>
+        </Grid>
+    );
 }
 
-export default UserDetailedPage;
+function query(props) {
+    return [
+        {
+            collection: 'users',
+            doc: props.match.params.id
+        }
+    ];
+}
+
+function mapState(state) {
+    const { users } = state.firestore.ordered;
+    const user = users && users.length ? users[0] : {};
+    return {
+        user
+    };
+}
+
+export default compose(
+    firestoreConnect(query),
+    connect(mapState)
+)(UserDetailedPage);
